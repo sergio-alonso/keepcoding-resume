dd096b0f2431664316c45787ca9712c39140fe3a
Sergio Alonso <sergio@sergioalonso.es>
2017-01-08 20:41:34 +0100

    Use the jQuery Ajax API to handle server data

---
+++ js/resume-ajax.js
@@ -0,0 +1,4 @@
+// http://api.jquery.com/category/ajax/
+//
+// The jQuery library has a full suite of Ajax capabilities.
+// The functions and methods therein allow us to load data from the server without a browser page refresh.
