c35a66fa9835e458074374c904e519ab65f21001
Sergio Alonso <sergio@sergioalonso.es>
2017-01-08 20:52:34 +0100

    The document ready event

--- js/resume-ajax.js
+++ js/resume-ajax.js
@@ -2,3 +2,7 @@
 //
 // The jQuery library has a full suite of Ajax capabilities.
 // The functions and methods therein allow us to load data from the server without a browser page refresh.
+//
+$(document).ready(function () {
+  // jQuery methods go here...
+});
