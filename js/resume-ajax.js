ea4f12baa7197ac917297b8552237885dd8fe47d
Sergio Alonso <sergio@sergioalonso.es>
2017-01-09 13:21:09 +0100

    Load title from JSON data

--- js/resume-ajax.js
+++ js/resume-ajax.js
@@ -6,6 +6,9 @@
 // The jQuery library has a full suite of Ajax capabilities.
 // The functions and methods therein allow us to load data from the server without a browser page refresh.
 //
+var url = "http://localhost:3000/db";
 $(document).ready(function () {
-  $("body").load("http://localhost:3000/db");
+  $.getJSON(url, function (data) {
+    $("title").html(data.basics.name);
+  });
 });
