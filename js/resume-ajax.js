08051147254dbef1363bd2fd32798a729c140e71
Sergio Alonso <sergio@sergioalonso.es>
2017-01-12 00:39:15 +0100

    Load timeline data

--- js/resume-ajax.js
+++ js/resume-ajax.js
@@ -18,6 +18,7 @@ $(document).ready(function () {
     $("footer .website").html(data.basics.website);
   });
   $.getJSON(url_work, function (data) {
+    var workTimeline = [];
     var slider_items = $('<ul/>');
     $.each(data, function (i, item) {
       var experience = $('<li/>', {
@@ -26,6 +27,12 @@ $(document).ready(function () {
           .summary
       });
       slider_items.prepend(experience);
+      var start = new Date(item.startDate).getFullYear();
+      var end = new Date(item.endDate).getFullYear();
+      if (!end) {
+        end = new Date().getFullYear();
+      }
+      workTimeline += JSON.stringify({ 'name': item.position, 'start': start, 'end': end }) + ",";
     });
     var slider = $('<div/>', { class: "slider-content", html: slider_items });
     $("#experience .slider").append(slider);
