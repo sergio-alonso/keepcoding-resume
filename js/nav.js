a5ac6e807dc7703b53fae38b6bc3b0dfd3fbc35e
Sergio Alonso <sergio@sergioalonso.es>
2017-01-14 04:28:56 +0100

    Smooth scrool on nav click

--- js/nav.js
+++ js/nav.js
@@ -1,4 +1,7 @@
 $("document").ready(function ($) {
+  //
+  // move nav to top on scroll
+  //
   var offsets = $('nav').offset().top;
   $(window).bind('scroll', function () {
     if ($(window).scrollTop() > offsets) {
@@ -6,5 +9,16 @@ $("document").ready(function ($) {
     } else {
       $("nav").css({ top: '6em', position: 'fixed' });
     }
+  });
+  //
+  // nav smooth scrool
+  //
+  $("nav ul li a").on('click', function (e) {
+    var url = e.target.href;
+    var hash = url.substring(url.indexOf("#") + 1);
+    $('html, body').animate({
+      scrollTop: $('#' + hash).offset().top
+    }, 500);
+    return false;
   });
 });
