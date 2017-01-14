72f2b69d611419e04e2b9a646f483b548d6ded65
Sergio Alonso <sergio@sergioalonso.es>
2017-01-14 02:11:46 +0100

    Dinamically add or remove a new input

--- js/resume-hire-me.js
+++ js/resume-hire-me.js
@@ -1,1 +1,11 @@
-$(document).ready(function () {});
+// Dinamically add or remove a new input depending on select option
+$(document).ready(function () {
+  $("#hire-me form select").on('change', function () {
+    if ("others" == this.value) {
+      $("<input type='text' name='other' id='other' placeholder='How?' />").insertAfter(this);
+    } else {
+      $('#other').remove();
+    }
+    return false;
+  });
+});
